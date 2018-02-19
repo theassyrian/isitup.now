20ed7f62ed182980c70ef78ead906a1cc1a88562
k4m4 <nikolaskam@gmail.com>
2018-02-19 23:44:55 +0000

    🎉 Initial release

---
+++ index.js
@@ -0,0 +1,87 @@
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var fs           = require('fs');
+var session      = require('express-session');
+var isUp         = require('is-up');
+var validator    = require('validator');
+var sanitizer    = require('express-sanitizer');
+var helmet       = require('helmet');
+var dotenv       = require('dotenv')
+var app          = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static(__dirname));
+app.use(express.static("public"));
+
+app.disable('x-powered-by')
+app.use(sanitizer())
+app.use(helmet());
+dotenv.load();
+app.use(session({
+    secret: process.env.SECRET,
+    cookie: {
+        httpOnly: true, 
+        secure: true
+    },
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/check', function(req, res, next) {
+    site = validator.escape(req.sanitize(req.body.url).replace('https://', '').replace('http://', ''));
+
+    if (!validator.isURL(site)) {
+        status = 'doesn\'t seem to be a valid URL.'
+
+        fs.readFile(__dirname + '/status.html', function(err, data) {
+            if (err) {
+                throw err;
+            }
+
+            var html = data.toString();
+            var html = html.replace(/{{SITE_LINK}}/g, site);
+            var html = html.replace(/{{SITE}}/g, site);
+            var html = html.replace(/{{STATUS}}/g, status);
+
+            return res.send(html);
+        });
+    } else {
+        isUp(site).then(up => {
+            if (up == true) {
+                status = 'is up!';
+            } else {
+                status = 'seems to be down!';
+            }
+    
+            fs.readFile(__dirname + '/status.html', function(err, data) {
+                if (err) {
+                    throw err;
+                }
+    
+                var html = data.toString();
+                var site_link = 'http://' + site;
+                var html = html.replace(/{{SITE_LINK}}/g, site_link);
+                var html = html.replace(/{{SITE}}/g, site);
+                var html = html.replace(/{{STATUS}}/g, status);
+    
+                res.send(html);
+            });
+        }).catch(next);
+    }
+});
+
+app.use(function (err, req, res, next) {
+    console.log(err)
+    res.status(500).send('Something broke!')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
---
+++ readme.md
@@ -0,0 +1,60 @@
+<h1 align="center">
+	<img width="500" src="https://nikolaskama.me/content/images/2018/02/isitup.png" alt="IsItUp.Now">
+</h1>
+
+<p align="center">A minimal service to check whether a site is 🆙 or broken.</p>
+
+---
+
+<p align="center">
+	<sub>Visit <a href="https://isitup.now.sh"><code>isitup.now.sh</code></a> for a live demo. Check out my <a href="https://nikolaskama.me">blog</a> and follow me on <a href="https://twitter.com/nikolaskama">Twitter</a>.</sub>
+</p>
+
+
+<br>
+
+# Installation
+
+```
+~ ❯❯❯ git clone https://github.com/k4m4/isitup.now
+~ ❯❯❯ cd isitup.now/
+~/isitup.now ❯❯❯ npm install
+~/isitup.now ❯❯❯ npm start
+```
+
+- You can then access the service by navigating to `localhost:3000`.
+
+
+<br>
+
+# Deployment
+
+First, [download `now`](https://zeit.co/download):
+
+```
+~ ❯❯❯ npm install -g now
+```
+
+Then, run `now` from *within* the directory of IsItUp.Now:
+
+```
+~/isitup.now ❯❯❯ now
+```
+
+<br>
+
+# Credits
+- IsItUp.Now is basically a remake of [isitup.org](https://github.com/sjparkinson/isitup.org) in Node.js, using Express. 
+- It was developed as part of my Node.js learning experience. I have uploaded the code with the intention of helping out others who are also trying to learn the node environment.
+- Most of the styling was adapted from [Evil Rabbit](https://twitter.com/evilrabbit_)'s [front site](https://github.com/evilrabbit/front).
+
+
+<br>
+
+# License
+
+Copyright (c) 2017 by Nikolaos Kamarinakis. Some rights reserved.
+
+`Donations` is under the terms of the **MIT License**, following all clarifications stated in the [license file](license.md).
+
+For more information on this project you can go ahead and email me anytime at **nikolaskam{at}gmail{dot}com**.
